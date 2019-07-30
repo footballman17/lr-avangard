e99d59d13cf21d6a431009debdad7327f1eb45c8
Valery Churbanov <valery@churbanov74.ru>
2019-07-30 15:17:33 +0400

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const conf = {
+    // точка входа
+    entry: './src/index.js',
+    // итоговое название файла
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'main.js',
+        publicPath: 'dist/',
+    },
+};
+
+module.exports = conf;

6c3b699a034ddaf70065542b311523369b793a5c
Valery Churbanov <valery@churbanov74.ru>
2019-07-30 16:20:57 +0400

    Fix and run webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 const path = require('path');
+
 const conf = {
-    // точка входа
-    entry: './src/index.js',
-    // итоговое название файла
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'main.js',
-        publicPath: 'dist/',
-    },
+  // точка входа
+  entry: './src/index.js',
+  // итоговое название файла
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'main.js',
+    publicPath: 'dist/',
+  },
 };
 
 module.exports = conf;

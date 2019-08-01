44f4507726566f20c12406f071cdd6a4d8bc0727
Valery Churbanov <valery@churbanov74.ru>
2019-08-01 13:59:00 +0400

    Fix HMR for pug-code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import './scss/style.scss';
 import $ from 'jquery';
 
-const bodyHtml = require('./index.pug');
+import htmlHead from './pug/head.pug';
+import htmlBody from './pug/body.pug';
 
-$('body').html(bodyHtml);
+const head = $('head');
+const body = $('body');
+
+head.children(':not(style)').remove();
+head.prepend(htmlHead);
+
+body.children(':not(:last)').remove();
+body.prepend(htmlBody);
 
 if (module.hot) {
   module.hot.accept();

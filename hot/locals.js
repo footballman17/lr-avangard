505c3f0b468fe9a9bfd4600130f593b6b5699462
ft17 <valery@churbanov74.ru>
2019-11-11 18:15:32 +0300

    Add some pug-variables

--- hot/locals.js
+++ hot/locals.js
@@ -4,6 +4,10 @@ const locals = {
   nameSite: 'lr-avangard.ru',
   projectTitle: 'Новый сервис Ленд Ровер на ТТК в Москве',
   pageTitle: 'Страница сайта lr-avangard',
+  userPhone: '+7(495) 241-29-39',
+  phoneLink: 'tel:+74952412939',
+  userMail: 'info@lr-avangard.ru',
+  userAddress: 'Москва, ул. Двинцев 3с6',
 };
 
 export default locals;
